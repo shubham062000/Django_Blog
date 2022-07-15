e1a9ac76faa69ff7e0031b745d7fb1b61122e9d1
shubham062000 <join.shubhamtorkade@gmail.com>
2022-07-15 16:51:44 +0530

    updated

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.CreateBlog, name = 'blog'),
-    path('blogs/', views.Blogs, name= 'blogs')
+    path('blogs/', views.Blogs, name= 'blogs'),
+    path('update/<int:id>', views.update,name= 'update'),  
+    path('delete/<int:id>', views.destroy,name= 'delete'), 
+    
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BlogForm
 from .models import Blog
 
@@ -9,6 +9,7 @@ def CreateBlog(request):
     form = BlogForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
+        return redirect('blogs')
  
     context['form']= form
     return render(request, "blog.html", context)
@@ -16,3 +17,16 @@ def CreateBlog(request):
 def Blogs(request):
     blogs = Blog.objects.all()
     return(render(request, 'blogs.html', {'blogs':blogs}))
+
+def update(request, id):   
+    blog = get_object_or_404(Blog, id = id)
+    form = BlogForm(request.POST or None, instance = blog)
+    if form.is_valid():
+        form.save()
+        return redirect("blogs") 
+    return render(request, "blog.html", {'form':form})
+
+def destroy(request, id):  
+    blog = Blog.objects.get(id=id)  
+    blog.delete()  
+    return redirect("blogs")
